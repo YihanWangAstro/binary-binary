320e8bbb530689f6018e493d560d0360ed5d02b3
Yihan <yihan.wang.1@stonybrook.edu>
2020-03-09 00:52:52 -0400

    Fix E simulations

--- code/binary-binary.cpp
+++ code/binary-binary.cpp
@@ -92,6 +92,79 @@ auto create_iso_jupiter_system() {
   return std::make_tuple(sun, jupiter, jupiter_orbit);
 }*/
 
+void binary_binary_Adrain(size_t th_id, std::string const &dir, size_t sim_num, double a_s, double E, double Q) {
+  char name[100];
+  sprintf(name, "%.0lf-%.2lf-%d.txt", a_s, E, static_cast<int>(th_id));
+
+  std::fstream out_file{dir + "two-binaries-" + name, std::fstream::out};
+
+  double const tidal_factor = 1e-5;
+
+  double inc = 0.0;
+
+  double A = Q / (1 - E);
+
+  for (size_t i = 0; i < sim_num; ++i) {
+    bool is_collided = false;
+
+    Particle star1{1_Ms, 1_Rs}, star2{1_Ms, 1_Rs};
+
+    auto [sun, jupiter, jupiter_orbit] = create_jupiter_system(inc);
+
+    auto binary_orbit = EllipOrbit{star1.mass, star2.mass, a_s, 0, inc, isotherm, isotherm, isotherm};
+
+    move_particles(binary_orbit, star2);
+
+    auto const w = consts::pi * static_cast<int>(random::Uniform(0, 2));  // random::Uniform(0, 2 * consts::pi);
+
+    auto const r_start = orbit::tidal_radius(tidal_factor, M_tot(sun, jupiter), M_tot(star1, star2),
+                                             2 * jupiter_orbit.a, 2 * binary_orbit.a);
+
+    double V = sqrt(-M_tot(sun, jupiter, star1, star2) / A);
+
+    double B = -A * sqrt(E * E - 1);
+
+    auto const in_orbit =
+        orbit::HyperOrbit(M_tot(sun, jupiter), M_tot(star1, star2), V, B, w, 0, 0, r_start, orbit::Hyper::in);
+
+    move_particles(in_orbit, star1, star2);
+
+    move_to_COM_frame(sun, jupiter, star1, star2);
+
+    double end_time = 2 * time_to_periapsis(cluster(sun, jupiter), cluster(star1, star2));
+
+    spacex::SpaceXsim::RunArgs args;
+
+    if constexpr (coll_detect) {
+      args.add_stop_condition([&](auto &ptc) -> bool {
+        size_t number = ptc.number();
+        for (size_t i = 0; i < number; ++i) {
+          for (size_t j = i + 1; j < number; ++j) {
+            if (ptc.Collide(i, j)) {
+              is_collided = true;
+              return true;
+            }
+          }
+        }
+        return false;
+      });
+    }
+
+    args.add_stop_condition(end_time);
+
+    args.add_stop_point_operation([&](auto &ptc) {
+      auto [a_j, e_j, a_s, e_s] = post_ae(ptc);
+
+      space::display(out_file, i, w, is_collided, a_j, e_j, a_s, e_s);
+      out_file << std::endl;
+    });
+
+    spacex::SpaceXsim simulator{0, sun, jupiter, star1, star2};
+
+    simulator.run(args);
+  }
+}
+
 void binary_binary(size_t th_id, std::string const &dir, size_t sim_num, double a_s, double b_factor) {
   char name[100];
   sprintf(name, "%.0lf-%.0lf.txt", a_s, 2 * b_factor);
@@ -113,7 +186,7 @@ void binary_binary(size_t th_id, std::string const &dir, size_t sim_num, double 
 
     auto const b = (jupiter_orbit.a + 0.5 * binary_orbit.a) * b_factor;
 
-    auto const w = consts::pi * static_cast<int>(random::Uniform(0, 2));//random::Uniform(0, 2 * consts::pi);
+    auto const w = consts::pi * static_cast<int>(random::Uniform(0, 2));  // random::Uniform(0, 2 * consts::pi);
 
     auto const r_start = orbit::tidal_radius(tidal_factor, M_tot(sun, jupiter), M_tot(star1, star2),
                                              2 * jupiter_orbit.a, 2 * binary_orbit.a);
@@ -157,18 +230,28 @@ int main(int argc, char **argv) {
   // std::array<double, 1> incs = {0_deg};
   // std::array<double, 4> a_s = {1_AU, 5_AU, 25_AU, 125_AU};
   // std::array<double, 7> b_factors = {0.125, 0.25, 0.5, 1, 2, 4, 8};
-
   std::vector<std::thread> threads;
 
   size_t th_id = 0;
-  double b_factor_max = 20;
-  double db = 0.5;
-  // for (auto a : a_s) {
-  // for (auto b : b_factors) {
-  for (double b = 0.5; b <= b_factor_max; b += db) {
-    threads.emplace_back(std::thread{binary_binary, th_id, output_dir, sim_num, a, b});
+  double min_Q = 1_AU;
+  double max_Q = 1000_AU;
+  double E = 1.5;
+
+  double dp = (log10(max_Q) - log10(min_Q)) / 40;
+
+  for (double p = 0; p <= log10(max_Q); p += dp) {
+    threads.emplace_back(std::thread{binary_binary_Adrain, th_id, output_dir, sim_num, a, E, pow(10.0, p)});
     th_id++;
   }
+
+  // double b_factor_max = 20;
+  // double db = 0.5;
+  // for (auto a : a_s) {
+  // for (auto b : b_factors) {
+  /*for (double b = 0.5; b <= b_factor_max; b += db) {
+    threads.emplace_back(std::thread{binary_binary, th_id, output_dir, sim_num, a, b});
+    th_id++;
+  }*/
   //}
 
   for (auto &th : threads) {
