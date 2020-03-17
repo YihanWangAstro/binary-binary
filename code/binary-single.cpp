b52ba246bbd5a0a9921aeb711561fab09bc83921
Yihan <yihan.wang.1@stonybrook.edu>
2020-03-17 03:46:57 -0400

    add binary-single case

--- code/binary-single.cpp
+++ code/binary-single.cpp
@@ -29,15 +29,9 @@ auto post_ae = [](auto const &ptc) {
 
   auto const &jup = ptc[1];
 
-  auto const &star1 = ptc[2];
-
-  auto const &star2 = ptc[3];
-
   auto const [a_j, e_j] = calc_a_RL_vector(star.m + jup.m, jup.pos - star.pos, jup.vel - star.vel);
 
-  auto const [a_s, e_s] = calc_a_RL_vector(star1.m + star2.m, star1.pos - star2.pos, star1.vel - star2.vel);
-
-  return std::make_tuple(a_j, e_j, a_s, e_s);
+  return std::make_tuple(a_j, e_j);
 };
 
 auto jupiter_ejection = [](auto const &ptc) -> bool {
@@ -92,13 +86,13 @@ auto create_iso_jupiter_system() {
   return std::make_tuple(sun, jupiter, jupiter_orbit);
 }*/
 
-void binary_binary_Adrain(size_t th_id, std::string const &dir, size_t sim_num, double a_s, double E, double Q) {
+void binary_single_Adrain(size_t th_id, std::string const &dir, size_t sim_num, double a_s, double E, double Q) {
   char name[100];
   sprintf(name, "%.0lf-%.2lf-%d.txt", a_s, E, static_cast<int>(th_id));
 
-  std::fstream out_file{dir + "two-binaries-" + name, std::fstream::out};
+  std::fstream out_file{dir + "binary-single-" + name, std::fstream::out};
 
-  std::fstream state_file{dir + "state-" + name, std::fstream::out};
+  std::fstream state_file{dir + "bs-state-" + name, std::fstream::out};
 
   double const tidal_factor = 1e-7;
 
@@ -109,32 +103,27 @@ void binary_binary_Adrain(size_t th_id, std::string const &dir, size_t sim_num, 
   for (size_t i = 0; i < sim_num; ++i) {
     bool is_collided = false;
 
-    Particle star1{1_Ms, 1_Rs}, star2{1_Ms, 1_Rs};
+    Particle star1{1_Ms, 1_Rs};
 
     auto [sun, jupiter, jupiter_orbit] = create_jupiter_system(inc);
 
-    auto binary_orbit = EllipOrbit{star1.mass, star2.mass, a_s, 0, inc, isotherm, isotherm, isotherm};
-
-    move_particles(binary_orbit, star2);
-
     auto const w = consts::pi * static_cast<int>(random::Uniform(0, 2));  // random::Uniform(0, 2 * consts::pi);
 
-    auto const r_start = std::max(orbit::tidal_radius(tidal_factor, M_tot(sun, jupiter), M_tot(star1, star2),
-                                                      2 * jupiter_orbit.a, 2 * binary_orbit.a),
-                                  5 * Q);
+    auto const r_start = std::max(
+        orbit::tidal_radius(tidal_factor, M_tot(sun, jupiter), star1.mass, 2 * jupiter_orbit.a, 2 * star1.radius),
+        5 * Q);
 
-    double V = sqrt(-M_tot(sun, jupiter, star1, star2) / A);
+    double V = sqrt(-M_tot(sun, jupiter, star1) / A);
 
     double B = -A * sqrt(E * E - 1);
 
-    auto const in_orbit =
-        orbit::HyperOrbit(M_tot(sun, jupiter), M_tot(star1, star2), V, B, w, 0, 0, r_start, orbit::Hyper::in);
+    auto const in_orbit = orbit::HyperOrbit(M_tot(sun, jupiter), star1.mass, V, B, w, 0, 0, r_start, orbit::Hyper::in);
 
-    move_particles(in_orbit, star1, star2);
+    move_particles(in_orbit, star1);
 
-    move_to_COM_frame(sun, jupiter, star1, star2);
+    move_to_COM_frame(sun, jupiter, star1);
 
-    double end_time = 2 * time_to_periapsis(cluster(sun, jupiter), cluster(star1, star2));
+    double end_time = 2 * time_to_periapsis(cluster(sun, jupiter), star1);
 
     spacex::SpaceXsim::RunArgs args;
 
@@ -156,77 +145,20 @@ void binary_binary_Adrain(size_t th_id, std::string const &dir, size_t sim_num, 
     args.add_stop_condition(end_time);
 
     args.add_stop_point_operation([&](auto &ptc) {
-      auto [a_j, e_j, a_s, e_s] = post_ae(ptc);
+      auto [a_j, e_j] = post_ae(ptc);
 
-      space::display(out_file, i, w, is_collided, a_j, e_j, a_s, e_s);
+      space::display(out_file, i, w, is_collided, a_j, e_j);
       out_file << std::endl;
 
       space::display(state_file, i, w, is_collided, ptc, "\r\n");
     });
 
-    spacex::SpaceXsim simulator{0, sun, jupiter, star1, star2};
+    spacex::SpaceXsim simulator{0, sun, jupiter, star1};
 
     simulator.run(args);
   }
 }
 
-/*
-void binary_binary(size_t th_id, std::string const &dir, size_t sim_num, double a_s, double b_factor) {
-  char name[100];
-  sprintf(name, "%.0lf-%.0lf.txt", a_s, 2 * b_factor);
-
-  std::fstream out_file{dir + "two-binaries-" + name, std::fstream::out};
-
-  double const tidal_factor = 1e-5;
-
-  double inc = 0.0;
-
-  for (size_t i = 0; i < sim_num; ++i) {
-    Particle star1{1_Ms, 1_Rs}, star2{1_Ms, 1_Rs};
-
-    auto [sun, jupiter, jupiter_orbit] = create_jupiter_system(inc);
-
-    auto binary_orbit = EllipOrbit{star1.mass, star2.mass, a_s, 0, inc, isotherm, isotherm, isotherm};
-
-    move_particles(binary_orbit, star2);
-
-    auto const b = (jupiter_orbit.a + 0.5 * binary_orbit.a) * b_factor;
-
-    auto const w = consts::pi * static_cast<int>(random::Uniform(0, 2));  // random::Uniform(0, 2 * consts::pi);
-
-    auto const r_start = orbit::tidal_radius(tidal_factor, M_tot(sun, jupiter), M_tot(star1, star2),
-                                             2 * jupiter_orbit.a, 2 * binary_orbit.a);
-
-    auto const in_orbit =
-        orbit::HyperOrbit(M_tot(sun, jupiter), M_tot(star1, star2), v_inf, b, w, 0, 0, r_start, orbit::Hyper::in);
-
-    move_particles(in_orbit, star1, star2);
-
-    move_to_COM_frame(sun, jupiter, star1, star2);
-
-    double end_time = 2 * time_to_periapsis(cluster(sun, jupiter), cluster(star1, star2));
-
-    spacex::SpaceXsim::RunArgs args;
-
-    if constexpr (coll_detect) {
-      args.add_stop_condition(collision);
-    }
-
-    args.add_stop_condition(end_time);
-
-    args.add_stop_point_operation([&](auto &ptc) {
-      auto [a_j, e_j, a_s, e_s] = post_ae(ptc);
-
-      space::display(out_file, i, w, a_j, e_j, a_s, e_s);
-      out_file << std::endl;
-    });
-
-    spacex::SpaceXsim simulator{0, sun, jupiter, star1, star2};
-
-    simulator.run(args);
-  }
-}
-*/
 int main(int argc, char **argv) {
   size_t sim_num;
   std::string output_dir;
@@ -246,7 +178,7 @@ int main(int argc, char **argv) {
   double dp = (log10(max_Q) - log10(min_Q)) / 40;
 
   for (double p = 0; p <= log10(max_Q); p += dp) {
-    threads.emplace_back(std::thread{binary_binary_Adrain, th_id, output_dir, sim_num, a, E, pow(10.0, p)});
+    threads.emplace_back(std::thread{binary_single_Adrain, th_id, output_dir, sim_num, a, E, pow(10.0, p)});
     th_id++;
   }
 
